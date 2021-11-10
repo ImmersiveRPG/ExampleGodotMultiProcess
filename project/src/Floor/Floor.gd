aa45ce5f914b0ab76e71c4f9b3ab660e0fcdaf41
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2021-11-10 07:53:27 -0800

    Removed unused code

--- project/src/Floor/Floor.gd
+++ project/src/Floor/Floor.gd
@@ -4,10 +4,4 @@
 
 extends StaticBody
 
-var _peer_id : int = -1
 
-func _on_Area_body_entered(body : PhysicsBody) -> void:
-	if body is NPC:
-		var marker = self
-		print("marker: %s" % [marker.name])
-		Server.transfer_to_peer(body, _peer_id)
