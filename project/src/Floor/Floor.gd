0e9addac7aad6a2c22da7f0962ed0d5e43fc1077
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2021-11-10 11:05:25 -0800

    Changed floor tiles to be different colors

--- project/src/Floor/Floor.gd
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2021 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
-# This file is license under the MIT License
-# https://github.com/ImmersiveRPG/ExampleGodotMultiProcess
-
-extends StaticBody
-
-
