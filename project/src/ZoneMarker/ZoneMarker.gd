07141f4989a485c9291ca2b25791e380fa6cad0d
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2021-11-09 16:37:45 -0800

    Made NPC transfer between server and client

--- project/src/ZoneMarker/ZoneMarker.gd
+++ project/src/ZoneMarker/ZoneMarker.gd
@@ -3,6 +3,7 @@ extends Area
 var _peer_id : int = -1
 
 func _on_Area_body_entered(body : PhysicsBody) -> void:
+	print("body entered: %s" % [body.name])
 	if body is NPC:
 		var marker = self
 		print("marker: %s" % [marker.name])
